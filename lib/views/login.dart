c4dc44a87a2958573cf7254a9cac18ed0b015ecf
claudiostocco <claudiostocco@gmail.com>
2022-02-13 19:27:35 -0300

    Implementing login view.

--- lib/views/login.dart
+++ lib/views/login.dart
@@ -7,22 +7,41 @@ class LoginCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    _textEditRow({required IconData icon, required String inputLabel}) {
+      return Row(
+        children: [
+          Icon(icon), 
+          const TextField(
+            decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              labelText: inputLabel,
+            ),
+          ),
+        ],
+      );
+    }
     _login() {
       Navigator.pushNamed(context, '/home');
     }
 
     return Container(
       padding: const EdgeInsets.all(30),
+      width: MediaQuery.of(context).size.width,
+      height: MediaQuery.of(context).size.height,
+      color: Colors.amber,
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           Expanded(child: Image.asset('assets/images/cadeado.png')),
-          Expanded(
-            child: ElevatedButton(
-              onPressed: _login,
-              child: const Text('Entrar'),
-            ),
-          )
+          // Expanded(child: SizedBox(width: MediaQuery.of(context).size.width, child: Row(children: const [Icon(Icons.email), TextField(),],))),
+          // Expanded(child: _textEditRow(icon: Icons.email, label: 'Email')),
+      //     Expanded(child: _textEditRow(icon: Icons.password, label: 'Senha')),
+      //     Expanded(
+      //       child: ElevatedButton(
+      //         onPressed: _login,
+      //         child: const Text('Entrar'),
+      //       ),
+      //     )
         ],
       ),
     );
@@ -42,6 +61,7 @@ class Login extends StatelessWidget {
           child: Image.asset(
             'assets/images/background.jpg',
             fit: BoxFit.cover,
+            alignment: Alignment.topCenter,
           ),
         ),
         const LoginCard(),
