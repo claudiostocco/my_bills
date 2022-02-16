f1ac8e31deb50fa61eb79255d8cc4756aa46d2f8
developer 203 <claudiostocco@gmail.com>
2022-02-16 16:46:34 -0300

    Validating email.

--- lib/controllers/app_controller.dart
+++ lib/controllers/app_controller.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import '../constants/constants.dart';
 import '../types/user_data.dart';
 
 class AppController extends ChangeNotifier {
@@ -8,10 +9,11 @@ class AppController extends ChangeNotifier {
   UserData? userData;
 
   bool login(String user, String pass, void Function(String error)? onError) {
-    if (user.isEmpty || pass.isEmpty || !user.contains('@')) {
+    if (user.isEmpty || pass.isEmpty || !RegExp(kRegExpIsValidEmail).hasMatch(user)) {
       if (onError != null) onError('Usuário ou senha inválidos!');
       return false;
     }
+
     userData = const UserData(
       'claudiostocco@gmail.com',
       'Claudio Marcio Stocco',
